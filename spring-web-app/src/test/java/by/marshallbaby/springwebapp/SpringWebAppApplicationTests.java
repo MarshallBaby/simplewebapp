b090b3f85cfb10561162036f46421029c8927f65
Alexey Yatsevich <76121473+MarshallBaby@users.noreply.github.com>
2022-02-17 22:29:34 +0300

    init

---
+++ spring-web-app/src/test/java/by/marshallbaby/springwebapp/SpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.marshallbaby.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
