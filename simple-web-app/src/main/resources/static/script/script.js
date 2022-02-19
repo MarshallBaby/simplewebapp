33084d3529b071b3cebf40a3ea4077d24254c1dd
Alexey Yatsevich <76121473+MarshallBaby@users.noreply.github.com>
2022-02-19 23:53:12 +0300

    UI Init

--- simple-web-app/src/main/resources/static/script/script.js
+++ simple-web-app/src/main/resources/static/script/script.js
@@ -1,3 +1,48 @@
+const switchEnum = Object.freeze({
+  ADD: 0,
+  EDIT: 1,
+  REMOVE: 2,
+  SEARCH: 3,
+});
+
+let switchState = switchEnum.ADD;
+
+let addBtn = document.getElementById('addBtn');
+let editBtn = document.getElementById('editBtn');
+let removeBtn = document.getElementById('removeBtn');
+let searchBtn = document.getElementById('searchBtn');
+
+function switchHandler(statement){
+  switch (statement) {
+    case switchEnum.ADD:
+        {
+          switchState = switchEnum.ADD;
+          addBtn.classList.add('switch-add-active');
+
+          editBtn.classList.remove('switch-edit-active');
+          removeBtn.classList.remove('switch-remove-active');
+          searchBtn.classList.remove('switch-search-active');
+        }
+      break;
+      case switchEnum.EDIT:
+          {
+            switchState = switchEnum.EDIT;
+            editBtn.classList.add('switch-edit-active');
+
+            addBtn.classList.remove('switch-add-active');
+            removeBtn.classList.remove('switch-remove-active');
+            searchBtn.classList.remove('switch-search-active');
+          }
+        break;
+    default:
+
+  }
+}
+
+
+switchHandler(switchEnum.ADD);
+
+
 formData = {
     "firstName" : "test jquery",
     "lastName" : "pliz rabotay",
@@ -7,7 +52,7 @@ formData = {
 }
 
 var settings = {
-    "url": "http://localhost:8080/api/employee",
+    "url": "http://192.168.1.120:8080/api/employee",
     "method": "POST",
     "data": JSON.stringify(formData),
     "headers": {
@@ -15,6 +60,6 @@ var settings = {
     }
 }
 
-$.ajax(settings).done(function (response) {
-    console.log(response);
-});
+// $.ajax(settings).done(function (response) {
+//     console.log(response);
+// });
