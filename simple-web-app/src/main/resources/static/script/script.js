c74ed4ed5fa76ad45118048cf69ed90475078187
Alexey Yatsevich <76121473+MarshallBaby@users.noreply.github.com>
2022-02-20 23:38:09 +0300

    UI Edit Page

--- simple-web-app/src/main/resources/static/script/script.js
+++ simple-web-app/src/main/resources/static/script/script.js
@@ -16,10 +16,14 @@ let confimBtn = document.getElementById('confirm-button');
 let confirmIcon = document.getElementById('confirm-icon');
 let confirmMsg = document.getElementById('confirm-message');
 
-function clearInputs(value){
+let idField = document.getElementById('add-employee-id');
+
+function clearInputs(clearId = false){
 
     document.querySelectorAll("input").forEach((item, i) => {
-      item.value = "";
+      if(item.getAttribute('id') != 'add-employee-id' || clearId == true){
+          item.value = "";
+      }
     });
 
 }
@@ -42,9 +46,16 @@ function processAjaxReply(currentCode, expectedCode, xhr){
   }
 }
 
+function btnColorRemove(){
+  addBtn.classList.remove("switch-add-active");
+  editBtn.classList.remove("switch-edit-active");
+  removeBtn.classList.remove("switch-remove-active");
+  searchBtn.classList.remove("switch-search-active");
+}
+
 function switchHandler(statement){
   switchState = statement;
-  clearInputs();
+  clearInputs(true);
   document.querySelectorAll(".field").forEach((item, i) => {
     item.classList.add("display-none");
   });
@@ -54,6 +65,8 @@ function switchHandler(statement){
         {
             confimBtn.style.backgroundColor = "#A981FF";
 
+            btnColorRemove();
+            addBtn.classList.add("switch-add-active");
 
             document.querySelectorAll(".add-field").forEach((item, i) => {
               item.classList.remove("display-none");
@@ -64,6 +77,8 @@ function switchHandler(statement){
           {
             confimBtn.style.backgroundColor = "#F1CB00";
 
+            btnColorRemove();
+            editBtn.classList.add("switch-edit-active");
 
             document.querySelectorAll(".edit-field").forEach((item, i) => {
               item.classList.remove("display-none");
@@ -95,6 +110,8 @@ function sendRequest(){
       case switchEnum.ADD:
             processSaveRequest();
         break;
+      case switchEnum.EDIT:
+            processEditRequest();
       default:
 
     }
@@ -109,7 +126,7 @@ function processSaveRequest(){
         "lastName" : document.getElementById('add-last-name').value,
         "departmentId" : document.getElementById('add-dep-id').value,
         "jobTitle" : document.getElementById('add-job-tag').value,
-        "gender" : "MALE"
+        "gender" : document.getElementById('add-gender').value
     }
 
     $.ajax({
@@ -120,11 +137,10 @@ function processSaveRequest(){
             "Content-Type": "application/json"
         },
         "complete": function(xhr, textStatus) {
-        console.log(xhr);
 
         clearInputs();
 
-        confirmMsg.classList.remove("display-none")
+        confirmMsg.classList.remove("display-none");
 
         if(xhr.status === 201){
           confirmMsg.textContent = "OK";
@@ -136,30 +152,113 @@ function processSaveRequest(){
           confirmMsg.textContent = xhr.statusText;
           confirmMsg.style.color = "#F1CB00";
         }
+
+        setTimeout(function(){
+          confirmMsg.classList.add("display-none");
+        }, 3000)
     }
       });
 
 
 }
 
+function processEditRequest(){
+  if(idField.value == ""){
+    clearInputs(true);
+    idField.classList.add('border-red');
 
-formData = {
-    "firstName" : "test jquery",
-    "lastName" : "pliz rabotay",
-    "departmentId" : 27,
-    "jobTitle" : "JS DEV",
-    "gender" : "MALE"
-}
+    setTimeout(function() {
+      idField.classList.remove('border-red');
+    }, 1000)
 
-var settings = {
-    "url": "http://192.168.1.120:8080/api/employee",
-    "method": "POST",
+    return;
+
+  }
+
+  let formData = {
+      "firstName" : document.getElementById('add-first-name').value,
+      "lastName" : document.getElementById('add-last-name').value,
+      "departmentId" : document.getElementById('add-dep-id').value,
+      "jobTitle" : document.getElementById('add-job-tag').value,
+      "gender" : document.getElementById('add-gender').value
+  }
+
+  $.ajax({
+    "url": "http://192.168.1.120:8080/api/employee/" + idField.value,
+    "method": "PUT",
     "data": JSON.stringify(formData),
     "headers": {
         "Content-Type": "application/json"
+    },
+    "complete": function(xhr, textStatus) {
+
+    clearInputs(true);
+
+    console.log(xhr);
+
+    confirmMsg.classList.remove("display-none");
+
+    if(xhr.status === 200){
+      confirmMsg.textContent = "OK";
+      confirmMsg.style.color = "#128300";
+    }else if(xhr.status === 0){
+      confirmMsg.textContent = "Connection error";
+      confirmMsg.style.color = "#F11D00";
+    }else{
+      confirmMsg.textContent = xhr.statusText;
+      confirmMsg.style.color = "#F1CB00";
     }
+
+    setTimeout(function(){
+      confirmMsg.classList.add("display-none");
+    }, 3000)
+}
+  })
 }
 
-// $.ajax(settings).done(function (response) {
-//     console.log(response);
-// });
+let globalTimeout = null;
+
+function processEditSearchRequest(){
+    let searchValue = idField.value;
+
+    if(globalTimeout != null){
+      clearTimeout(globalTimeout);
+    }
+
+    globalTimeout = setTimeout(
+      function() {
+
+        if(idField.value == ""){
+          return;
+        }
+
+        $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "http://192.168.1.120:8080/api/employee/" + idField.value,
+        complete: function(data) {
+            if(data.status != 200){
+              clearInputs();
+              return;
+            }
+
+            let result = data.responseJSON;
+
+            document.getElementById('add-first-name').value = result.firstName;
+            document.getElementById('add-last-name').value = result.lastName;
+            document.getElementById('add-dep-id').value = result.departmentId;
+            document.getElementById('add-job-tag').value = result.jobTitle;
+            document.getElementById('add-gender').value = result.gender;
+
+
+
+
+        }
+    });
+
+      }
+    , 700)
+}
+
+
+idField.addEventListener('keyup', processEditSearchRequest)
