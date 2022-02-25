28d5430f858533ced8fa1031a3e865402bf45f5a
Alexey Yatsevich <76121473+MarshallBaby@users.noreply.github.com>
2022-02-26 01:18:44 +0300

    sql__

--- employee.sql
+++ employee.sql
@@ -10,3 +10,21 @@ CREATE TABLE employee (
 	
 INSERT INTO employee (first_name, last_name, department_id, job_title, gender)
 	VALUES ('Lisa', 'Fregoso', 54, 'Development', CAST('FEMALE' as gender));
+INSERT INTO employee (first_name, last_name, department_id, job_title, gender)
+	VALUES ('David', 'Hatcher', 54, 'Development', CAST('MALE' as gender));
+INSERT INTO employee (first_name, last_name, department_id, job_title, gender)
+	VALUES ('John', 'Shipley', 54, 'Development', CAST('MALE' as gender));
+INSERT INTO employee (first_name, last_name, department_id, job_title, gender)
+	VALUES ('Leslie', 'Hurtado', 27, 'HR', CAST('FEMALE' as gender));
+INSERT INTO employee (first_name, last_name, department_id, job_title, gender)
+	VALUES ('Almeda', 'Meadows', 27, 'HR', CAST('FEMALE' as gender));
+INSERT INTO employee (first_name, last_name, department_id, job_title, gender)
+	VALUES ('Shirley', 'Cox', 27, 'HR', CAST('FEMALE' as gender));
+INSERT INTO employee (first_name, last_name, department_id, job_title, gender)
+	VALUES ('Keith', 'Lane', 14, 'Testing', CAST('MALE' as gender));
+INSERT INTO employee (first_name, last_name, department_id, job_title, gender)
+	VALUES ('Joann', 'Kline', 14, 'Testing', CAST('FEMALE' as gender));
+INSERT INTO employee (first_name, last_name, department_id, job_title, gender)
+	VALUES ('Ron', 'Sherwood', 14, 'Testing', CAST('MALE' as gender));
+INSERT INTO employee (first_name, last_name, department_id, job_title, gender)
+	VALUES ('Danny', 'Macek', 14, 'Testing', CAST('MALE' as gender));
