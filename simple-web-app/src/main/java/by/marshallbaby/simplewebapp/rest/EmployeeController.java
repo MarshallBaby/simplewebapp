98c9964448dec864f99022f654c65f61739129d3
alexey <aleksey.yatsevich@gmail.com>
2022-03-21 00:04:54 +0300

    Swagger

--- simple-web-app/src/main/java/by/marshallbaby/simplewebapp/rest/EmployeeController.java
+++ simple-web-app/src/main/java/by/marshallbaby/simplewebapp/rest/EmployeeController.java
@@ -3,6 +3,8 @@ package by.marshallbaby.simplewebapp.rest;
 import by.marshallbaby.simplewebapp.dto.Employee;
 import by.marshallbaby.simplewebapp.exception.ResourceNotFoundException;
 import by.marshallbaby.simplewebapp.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
@@ -20,6 +22,7 @@ import java.util.Optional;
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api")
+@Tag(name = "Employee API", description = "Provides CRUD operations with Employee")
 public class EmployeeController {
 
     @Autowired
@@ -37,11 +40,13 @@ public class EmployeeController {
 //        return new ResponseEntity<>(employeeService.save(employee), HttpStatus.CREATED);
 //    }
 
+    @Operation(summary = "Save Employees")
     @PostMapping("/employees")
     public ResponseEntity<Iterable<Employee>> saveAllEmployees(@RequestBody Iterable<@Valid Employee> employees) {
         return new ResponseEntity<>(employeeService.saveAll(employees), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Find all employees", description = "Also you're able to filter employees by first name and last name")
     @GetMapping("/employees")
     public ResponseEntity<Iterable<Employee>> findEmployees(
             @RequestParam(value = "first-name", required = false) String firstName,
@@ -62,17 +67,20 @@ public class EmployeeController {
         }
     }
 
+    @Operation(summary = "Get employee by ID")
     @GetMapping("/employees/{id}")
     public ResponseEntity<Optional<Employee>> findEmployeeById(@PathVariable("id") Long id) {
         return new ResponseEntity<>(employeeService.findById(id), HttpStatus.FOUND);
     }
 
+    @Operation(summary = "Detele employee by ID")
     @DeleteMapping("/employees/{id}")
     public ResponseEntity<String> deleteEmployeeById(@PathVariable("id") Long id) {
         employeeService.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Operation(summary = "Update employee if exists")
     @PutMapping("/employees")
     public ResponseEntity<Employee> updateEmployee(@Valid @RequestBody Employee employee) {
         return new ResponseEntity<>(employeeService.update(employee), HttpStatus.OK);
